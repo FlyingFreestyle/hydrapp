22f3efb79e746bc7229ca564deebf3ee1faa6845
FlyingFreestyle <139637440+FlyingFreestyle@users.noreply.github.com>
2023-09-04 19:19:24 +0200

    Ensure key is created on action and init

--- src/js/main.js
+++ src/js/main.js
@@ -3,28 +3,34 @@ const cnt = document.querySelector('.count--js')
 const addGlass = document.querySelector('.add-glass--js')
 const subGlass = document.querySelector('.subtract-glass--js')
 
-const key = new Date().toISOString().slice(0, 10)
 let data = {}
 
-if (localStorage['data'])
-{
-    data = JSON.parse(localStorage['data'])
-    console.log(data)
-    cnt.innerHTML = data[key]
-} else {
-    data[key] = 0
-    localStorage.setItem('data', JSON.stringify(data))
-    console.log(`No data found for ${key}. Setting to 0`)
-    cnt.innerHTML = data[key]
+function init() {
+    const key = new Date().toISOString().slice(0, 10)
+    if (localStorage['data'])
+    {
+        data = JSON.parse(localStorage['data'])
+        console.log(data)
+        cnt.innerHTML = data[key]
+    } else {
+        data[key] = 0
+        localStorage.setItem('data', JSON.stringify(data))
+        console.log(`No data found for ${key}. Setting to 0`)
+        cnt.innerHTML = data[key]
+    }
 }
 
+init()
+
 addGlass.addEventListener('click', () => {
+    const key = new Date().toISOString().slice(0, 10)
     data[key] = data[key] + 1
     localStorage.setItem('data', JSON.stringify(data))
     cnt.innerHTML = data[key]
 })
 
 subGlass.addEventListener('click', () => {
+    const key = new Date().toISOString().slice(0, 10)
     let val = parseInt(localStorage[key])
     if (data[key] > 0) {
         data[key] = data[key] - 1
