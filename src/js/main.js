322052bbe88b6f399716516bdd24ef9ee0e39d7c
FlyingFreestyle <139637440+FlyingFreestyle@users.noreply.github.com>
2023-09-05 13:05:56 +0200

    Simplify listeners

--- src/js/main.js
+++ src/js/main.js
@@ -38,10 +38,6 @@ function operate(num) {
 
 init()
 
-addGlass.addEventListener('click', function() {
-    operate(1)
-})
+addGlass.addEventListener('click', operate(1))
 
-subGlass.addEventListener('click', function() {
-    operate(-1)
-})
+subGlass.addEventListener('click', operate(-1))
