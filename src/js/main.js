03dc577e3c1229533e55bdfd5ee6146fa668628f
FlyingFreestyle <139637440+FlyingFreestyle@users.noreply.github.com>
2023-09-06 19:23:09 +0200

    Simplify key creation

--- src/js/main.js
+++ src/js/main.js
@@ -7,12 +7,7 @@ let data = {}
 let key = ""
 
 function updateKey() {
-    const now = new Date();
-    const year = now.getFullYear();
-    const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are zero-based
-    const day = String(now.getDate()).padStart(2, '0');
-
-    key = `${year}-${month}-${day}`;
+    key = new Date().toLocaleDateString();
 }
 
 function updateCounterAndStorage() {
